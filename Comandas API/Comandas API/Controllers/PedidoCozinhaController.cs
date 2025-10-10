59cc519105e18a070647174ab46a2ce4eebae34b
Pietro Puntel <pietrosamueldossantospuntel@gmail.com>
2025-10-10 08:41:16 -0300

    não tenho certeza mais fiz o put e o post

--- Comandas API/Comandas API/Controllers/PedidoCozinhaController.cs
+++ Comandas API/Comandas API/Controllers/PedidoCozinhaController.cs
@@ -54,12 +54,44 @@ namespace Comandas_API.Controllers
         [HttpPost]
         public void Post([FromBody] string value)
         {
+            //if (cardapio.Titulo.Length < 3)
+
+            //    return Results.BadRequest("O titulo deve ter no mínimo 3 caracteres");
+            //if (cardapio.Descricao.Length < 3)
+            //    return Results.BadRequest("A descricao deve ter no mínimo 3 caracteres");
+            //if (cardapio.Preco <= 0)
+            //    return Results.BadRequest("O preço deve ser maior que zero");
+            //var cardapioItem = new CardapioItem
+            //{
+            //    Id = PedidoCozinhaId.Count + 1,
+            //    Titulo = PedidoCozinhaId.Titulo,
+            //    Descricao = PedidoCozinhaId.Descricao,
+            //    Preco = PedidoCozinhaId.Preco,
+            //    PossuiPreparo = cardapio.PossuiPreparo
+            //};
+            ////adicionar o cardapio na lista
+            //cardapios.Add(cardapioItem);
+            //return Results.Created($"/api/cardapio/{cardapioItem.Id}", cardapioItem);
         }
 
-        // PUT api/<PedidoCozinhaController>/5
+
+
+
+        /// <summary>
+        /// Atualiza um item do cardápio
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="PedidoCozinha"></param>
+        // PUT api/<PedidiCozinhaController>/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
+            var PedidoCozinha = pedidos.FirstOrDefault(c => c.Id == id);
+            if (PedidoCozinha is null)
+                return Results.NotFound($"Pedido Cozinha do id {id} Nao encontrado");
+            PedidoCozinha.PedidoCozinhaId = PedidoCozinha.pedidos;
+            PedidoCozinha.ComandaItemId = PedidoCozinha.ComandaId;
+            return Results.NoContent();
         }
 
         // DELETE api/<PedidoCozinhaController>/5
